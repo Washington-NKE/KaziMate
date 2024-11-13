efe2f38f0ff2f45e255b858b7b8891bf43594988
Washington <washiem2022@gmail.com>
2024-11-13 10:03:05 +0300

    styling the buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+  const [isCompleteScreen, setIsCompleteScreen] = useState(false);
   return (
     <div className="App">
       <h1>My TODOs</h1>
+
+      <div className="todo-wrapper"> 
+        <div className="todo-input">
+          <div className="todo-input-item">
+            <label>Title</label>
+            <input type="text" placeholder="What's the task title?"/>
+          </div>
+
+          <div className="todo-input-item">
+            <label>Description</label>
+            <input type="text" placeholder="What's the description?"/>
+          </div>
+
+          <div className="todo-input-item">
+            <button type="button" className="primary-btn">Add</button>
+          </div>
+        </div>
+
+        <div className="button-area">
+          <button className={`secondary-btn ${isCompleteScreen === false && 'active'}`} onClick={()=>setIsCompleteScreen(false)}>Todo</button>
+          <button className={`secondary-btn ${isCompleteScreen === true && 'active'}`} onClick={()=>setIsCompleteScreen(true)}>Completed</button>
+        </div>
+
+        <div className="todo-list">
+          <div className="todo-list-item">
+            <h3>Task 1</h3>
+            <p>Description</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
